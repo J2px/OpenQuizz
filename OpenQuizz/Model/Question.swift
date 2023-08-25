bcc43fe4a07ca64413797e686e2673cc424f19c5
J2PX <jimmy.jermidi@gmail.com>
2023-08-25 12:36:57 +0200

    commit 25-08-2023

---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Jimmy JERMIDI on 22/08/2023.
+//
+
+import Foundation
+
+struct Question{
+var title = " "
+var isCorrect = false
+
+
+
+
+}
